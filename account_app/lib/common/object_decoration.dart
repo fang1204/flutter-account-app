4d44c9db5b28450c7c962396bd4d33e0ca108337
ennisH-T-N <107b33963@mailst.cjcu.edu.tw>
2022-02-10 14:29:55 +0800

    test

---
+++ account_app/lib/common/object_decoration.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+/*
+控制TextField_decoration樣式
+ */
+
+class textfield_decoration{
+  InputDecoration textInputDecoration([String labelText='' , String hintText='']){
+    return InputDecoration(
+      labelText: labelText,
+      hintText: hintText,
+      fillColor: Colors.white,
+      filled: true,
+      //點選輸入時顏色變換
+      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0),borderSide: BorderSide(color: Colors.pink.shade200)),
+      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0),borderSide: BorderSide(color: Colors.pink)),
+    );
+  }
+}
