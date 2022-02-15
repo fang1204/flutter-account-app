0669d1ef619b1ad5d091ab24dccd34a70037a5d8
Ennis0620 <ennis06205668@gmail.com>
2022-02-15 18:18:48 +0800

    Ting outline fix

--- account_app/lib/common/object_decoration.dart
+++ account_app/lib/common/object_decoration.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
+
 
 /*
 控制TextField_decoration樣式
  */
 
 class TextFieldDecoration{
-  InputDecoration textInputDecoration([String labelText='' , String hintText='']){
+  InputDecoration textInputDecoration([String labelText='' , String hintText='',int choice_IncomePay=0]){
     return InputDecoration(
       labelText: labelText,
       hintText: hintText,
       fillColor: Colors.white,
       filled: true,
       //點選輸入時顏色變換
-      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0),borderSide: BorderSide(color: Colors.pink.shade200)),
-      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0),borderSide: const BorderSide(color: Colors.pink)),
+      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0),borderSide: BorderSide(color: ColoerPicker(choice_IncomePay)[0])),
+      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0),borderSide: BorderSide(color: ColoerPicker(choice_IncomePay)[1])),
     );
+  }
+  ColoerPicker(int choice_IncomePay){
+    if(choice_IncomePay==0){
+      return [Color.fromRGBO(51, 102, 153, 1),Color.fromRGBO(51, 102, 153, 0.5)];
+    }
+    else{
+      return [Color.fromRGBO(153, 51, 51, 1),Color.fromRGBO(153, 51, 51, 0.5)];
+    }
   }
 }
